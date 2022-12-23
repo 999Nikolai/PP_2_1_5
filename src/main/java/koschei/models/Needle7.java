ef7697704d540b26eb65446aa6d23c11c1e63d5c
999Nikolai <90135496+999Nikolai@users.noreply.github.com>
2022-12-24 00:15:33 +0300

    PP_2_1_5

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,15 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+    private final Deth8 rabbi;
+    @Autowired
+    public Needle7(Deth8 rabbi) {
+        this.rabbi = rabbi;
+    }
 
     @Override
     public String toString() {
